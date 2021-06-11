b3d14c7d44410536890b7496cbe714090a1fa1d8
liyang <609045119@qq.com>
2021-06-11 09:57:02 +0800

    新增测试

--- person/src/main/java/com/example/demo/controller/PersonController.java
+++ person/src/main/java/com/example/demo/controller/PersonController.java
@@ -5,6 +5,8 @@ import com.example.demo.domain.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * Created by andy on 2017/10/16.
  */
@@ -24,6 +26,11 @@ public class PersonController {
         person = dao.save(person);
 
         return person.getId().toString();
+    }
+
+    @GetMapping(value = "/list")
+    public List<Person> get() {
+        return dao.findAll();
     }
 
 
--- person/src/test/java/com/example/demo/PersonApplicationTests.java
+++ person/src/test/java/com/example/demo/PersonApplicationTests.java
@@ -1,16 +1,27 @@
 package com.example.demo;
 
+import com.example.demo.utils.PhoneUtils;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.*;
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
 public class PersonApplicationTests {
 
-	@Test
-	public void contextLoads() {
+
+	public static void main(String[] args) {
+		System.out.println(PhoneUtils.blurPhone("18758009334"));
+		System.out.println(PhoneUtils.maskIdCard("142724199302151617"));
+
 	}
 
 }
